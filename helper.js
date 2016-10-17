f92ea44b3a8f97f422b3d50595b7a9936c30b695
Chase Starr <chasestarr@gmail.com>
2016-10-17 11:21:54 -0700

    add getAccessToken function to auth with github api

--- helper.js
+++ helper.js
@@ -1,18 +1,32 @@
-const JWT = require('jwt-simple');
+const jwt = require('jwt-simple');
 const integrationId = require('./config').INTEGRATION_ID;
 const keyPath = require('./config').KEY_PATH;
 
 const key = fs.readFileSync(keyPath);
 
-function integrationAuth() {
+function _integrationAuth() {
   const time = Math.round(Date.now() / 1000);
-  return JWT.encode({
+  return jwt.encode({
         iat: time,
         exp: time + 60,
         iss: integrationId,
     }, key, 'RS256');
 }
 
+function getAccessToken(id, cb) {
+  const options = {
+    url: `https://api.github.com/installations/${id}/access_tokens`,
+    headers: {
+      'User-Agent': 'GitVid',
+      Accept: 'application/vnd.github.machine-man-preview+json',
+      Authorization: `Bearer ${_integration_token()}`
+    }
+  };
+  request.post(options, (err, res, body) => {
+    cb(body);
+  });
+}
+
 module.exports = {
-  integrationAuth: integrationAuth
+  getAccessToken: getAccessToken
 }
