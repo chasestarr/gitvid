3086e0336744b63c4ed4cdcac74666eb1d3c045d
Chase Starr <chasestarr@gmail.com>
2016-10-18 10:40:29 -0700

    refactor comment function

--- integration.js
+++ integration.js
@@ -37,13 +37,17 @@ function comment(username, commentUrl) {
         },
       };
 
-      request.post(options, (err, res, body) => {
-        if (err) console.log('request failed:', err);
-        console.log('status code:', res.statusCode);
-        console.log('Comment successful!');
-      });
+      commentOnIssue(options);
     });
   })
+}
+
+function commentOnIssue(options) {
+  request.post(options, (err, res, body) => {
+    if (err) console.log('request failed:', err);
+    console.log('status code:', res.statusCode);
+    console.log('Comment successful!');
+  });
 }
 
 module.exports = {
