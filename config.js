8b6002730499dc3d8fa5652e5bf58a56ae3d4cda
Chase Starr <chasestarr@gmail.com>
2016-10-18 10:14:31 -0700

    update gitignore

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   INTEGRATION_ID: 451,
   KEY_PATH: '/Users/chasestarr/gitvid.2016-10-16.private-key.pem',
-  BASE_URL: 'https://28566dde.ngrok.io',
+  BASE_URL: 'https://gitvid.net',
   SSL_KEY: '/Users/chasestarr/server.key',
   SSL_CERT: '/Users/chasestarr/server.crt'
 }
