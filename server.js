8883a2bfdb0d84bb790b79db6634a43298d5e3c6
Chase Starr <chasestarr@gmail.com>
2016-10-17 20:12:25 -0700

    add https

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const handler = require('./routeHandler');
@@ -13,7 +15,16 @@ app.get('/token', handler.handleToken);
 app.get('/', (req, res) => res.status(200).render('index.html'));
 app.get('/:shortCode', handler.navToRoom);
 
-const port = process.env.PORT || 7000;
-app.listen(port, function() {
-    console.log('server started at:', port);
+// const port = process.env.PORT || 7000;
+// app.listen(port, () => {
+//     console.log('server started at:', port);
+// });
+
+const options = {
+    key: fs.readFileSync(require('./config').SSL_KEY),
+    cert: fs.readFileSync(require('./config').SSL_CERT)
+}
+
+https.createServer(options, app).listen(8000, () => {
+    console.log('https server listening on 8000');
 });
